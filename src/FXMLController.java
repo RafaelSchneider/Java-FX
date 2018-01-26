05839a50b76281f1aacd2ec8ae2fa5b4b986a434
Rafael Schneider <rss.schneider@gmail.com>
2018-01-25 22:12:26 -0200

    criado acao de pesquisa

--- src/FXMLController.java
+++ src/FXMLController.java
@@ -1,15 +1,24 @@
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import sun.font.TextLabel;
+
 
 public class FXMLController implements Initializable {
 
     @FXML
     private TextField txtPesquisa;
     
+    @FXML
+    private Button btPesquisar;
+    
+    @FXML
+    public void pesquisar(ActionEvent pesquisarClick){
+        txtPesquisa.setText("Você acaba de pesquisar um item");
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
